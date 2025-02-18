5052230301f5ee19cc2bc256f9480f23304a780c
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-02-18 21:09:58 +0530

    task and tasks

--- secoondclass/src/app/user/user.component.ts
+++ secoondclass/src/app/user/user.component.ts
@@ -1,12 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-interface User{
-  id: string;
-  avatar: string;
-  name: string;
-}
-
-
+import { User } from './user.model';
 
 
 
