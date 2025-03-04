07c1e11b4ea0ccaeb51d216690db40b1d3f85d00
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-03-04 20:03:05 +0530

    update

--- routingpages/routing/src/app/app.component.ts
+++ routingpages/routing/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { routes } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'routing';
 }
+
+@NgModule({
+
+  imports: [RouterOutlet, BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+
+})
+export class AppModule {}
--- routingpages/routing/src/app/app.routes.ts
+++ routingpages/routing/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+
 
 export const routes: Routes = [
-    { path: ''.component: HomeComponent },//default
-    {
-        path: 'about', component: AboutComponent 
-    },
-    {path:'**' , redirectTo: ''}//wildcard
+    { path:'', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent }
 ];
 
 
