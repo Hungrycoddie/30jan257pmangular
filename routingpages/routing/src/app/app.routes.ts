80fe677413c4744977d4206ee528cc109db7b951
Arun raj <106869558+Hungrycoddie@users.noreply.github.com>
2025-03-11 20:07:19 +0530

    routing update

--- routingpages/routing/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FirstComponent } from './first/first.component';
-import { AboutComponent } from './about/about.component';
-
-export const routes: Routes = [
-  {path:'first-component', component:FirstComponent},
-  {path:'about', component:AboutComponent}
-
-]
